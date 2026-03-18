5e93df1fc656b8a55dd05c1c282e7a8569e88298
zenzen26 <thamzien@gmail.com>
2026-03-18 16:55:19 +1100

    local test

--- vite.config.js
+++ vite.config.js
@@ -2,22 +2,9 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
-  base: '/birthday-game/',
   plugins: [react()],
   assetsInclude: ['**/*.glb', '**/*.gltf', '**/*.bin'],
   optimizeDeps: {
     exclude: ['@react-three/rapier']
-  },
-  build: {
-    outDir: 'dist',
-    rollupOptions: {
-      output: {
-        manualChunks(id) {
-          if (id.includes('node_modules/three')) return 'three'
-          if (id.includes('node_modules/@react-three/rapier')) return 'rapier'
-          if (id.includes('node_modules/@react-three')) return 'fiber'
-        }
-      }
-    }
   }
 })
